b5346a9151c123725bbfc9757003452fdfcc749b
ancosen <ancosen@gmail.com>
2014-07-20 16:53:59 +0200

    First Commit

---
+++ manifests/params.pp
@@ -0,0 +1,8 @@
+class servicemix::params {
+  $version = "5.0.1"
+  $family = "servicemix-5"
+  $smixname = "apache-servicemix"
+  $source = "http://archive.apache.org/dist/servicemix/${family}/${version}/${$smixname}-${version}.zip"
+  $path = "/opt/"
+  $tmp = "/tmp/"
+}
