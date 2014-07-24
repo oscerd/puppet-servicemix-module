c6965b05466c3eebed377f12490919003c958a40
ancosen <ancosen@gmail.com>
2014-07-24 09:35:04 +0200

    Parameters validation added

--- manifests/setup.pp
+++ manifests/setup.pp
@@ -1,9 +1,32 @@
   define servicemix::setup (
-  $version = "5.0.1",
-  $family = "servicemix-5",
-  $path = "/opt/",
-  $tmp = "/tmp/",
+  $version = undef,
+  $family = undef,
+  $installdir = undef,
+  $tmpdir = undef,
   ) {
+  
+  # Validate parameters
+  if ($version == undef) {
+    fail('version parameter must be set')
+  }
+
+  if ($family == undef) {
+    fail('family parameter must be set')
+  }
+  
+  if ($installdir == undef){
+    notify{'Install folder not specified, setting default install folder /opt/':}
+    $defined_installdir ='/opt/'
+  } else {
+    $defined_installdir = $installdir
+  }
+
+  if ($tmpdir == undef){
+    notify{'Temp folder not specified, setting default install folder /tmp/':}
+    $defined_tmpdir ='/tmp/'
+  } else {
+    $defined_tmpdir = $tmpdir
+  }
   
   $smixname = "apache-servicemix"
   $source = "http://archive.apache.org/dist/servicemix/${family}/${version}/${smixname}-${version}.zip"
@@ -13,24 +36,28 @@
       alias => unzip   
   }
 
+  # Resource Default for exec
+	Exec {
+	  path => ["/bin/", "/sbin/", "/usr/bin/", "/usr/sbin/"] }
+
   exec { 'retrieve_servicemix': 
-          command => "wget -q ${source} -P ${tmp}",
-          unless => "ls ${path}${smixname}-${version}/",
+          command => "wget -q ${source} -P ${defined_tmpdir}",
+          unless => "ls ${defined_installdir}${smixname}-${version}/",
           timeout => 1000 }
 
   exec { 'unzip_servicemix': 
-          command => "unzip ${tmp}${smixname}-${version}.zip -d ${tmp}",
+          command => "unzip ${defined_tmpdir}${smixname}-${version}.zip -d ${defined_tmpdir}",
           require => [ Exec['retrieve_servicemix'], 
                        Package[unzip] ],
-          unless => "ls ${path}${smixname}-${version}/" }
+          unless => "ls ${defined_installdir}${smixname}-${version}/" }
 
   exec { 'mv_servicemix': 
-          command => "mv ${tmp}${smixname}-${version}/ ${path}",
+          command => "mv ${defined_tmpdir}${smixname}-${version}/ ${defined_installdir}",
           require => Exec['unzip_servicemix'],
-          unless => "ls ${path}${smixname}-${version}/" }
+          unless => "ls ${defined_installdir}${smixname}-${version}/" }
           
   exec { 'clean_servicemix': 
-          command => "rm -rf ${tmp}${smixname}-${version}.zip",
+          command => "rm -rf ${defined_tmpdir}${smixname}-${version}.zip",
           require => Exec['mv_servicemix'],
           logoutput => "false" }
 }
--- tests/init.pp
+++ tests/init.pp
@@ -1,11 +1,8 @@
 # Just copy init.pp in your manifests directory. The module will download, unzip e move apache servicemix
 
-Exec {
-  path => ["/bin/", "/sbin/", "/usr/bin/", "/usr/sbin/"] }
-
 servicemix::setup { "servicemix":
   version => "5.0.1",
   family => "servicemix-5",
-  path => "/opt/",
-  tmp => "/tmp/"
+  installdir => "/opt/",
+  tmpdir => "/tmp/"
   }
